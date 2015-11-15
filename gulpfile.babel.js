a06df73fe27d667f448867f2d5d1477ce85a41d6
孙崇升 <sunchongsheng@meituan.com>
2015-11-15 19:18:56 +0800

    init

---
+++ gulpfile.babel.js
@@ -0,0 +1,15 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import autoprefixer from 'gulp-autoprefixer';
+
+gulp.task('sass', function() {
+    return gulp.src('./source/scss/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./source/css'))
+});
+
+gulp.task('default', ['sass'], function() {
+    gulp.watch('./source/scss/**/*.scss', ['sass']);
+    gulp.watch('./source/scss/jekyll.scss', ['sass']);
+});
