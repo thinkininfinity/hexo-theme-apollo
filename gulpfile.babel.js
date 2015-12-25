f6c4832765d9050cb88b5b8182473acd0f32b7d4
Sean Sun <pinggodstudio@gmail.com>
2015-12-25 10:41:48 +0800

    update gulp config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,14 +2,25 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 
+const path = {
+    sass: './source/scss',
+    css: './source/css'
+};
+
+const config = {
+    sass: {
+        outputStyle: 'compressed'
+    }
+};
+
 gulp.task('sass', function() {
-    return gulp.src('./source/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+    return gulp.src(`${path.sass}/*.scss`)
+        .pipe(sass(config.sass))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('./source/css'));
+        .pipe(gulp.dest(path.css));
 });
 
 gulp.task('default', ['sass'], function() {
-    gulp.watch('./source/scss/_partial/*.scss', ['sass']);
-    gulp.watch('./source/scss/*.scss', ['sass']);
+    gulp.watch(`.${path.sass}/_partial/*.scss`, ['sass']);
+    gulp.watch(`.${path.sass}/*.scss`, ['sass']);
 });
